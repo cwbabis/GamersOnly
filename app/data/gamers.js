c18a169b01e0801a97fc7d3fd1ae81254aaf091b
Babis <cwbabis@gmail.com>
2019-04-26 21:25:03 -0700

    Made Template Array

--- app/data/gamers.js
+++ app/data/gamers.js
@@ -1,17 +1,9 @@
-// Holds an array of gamer objects
-{
-    "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-            "scores": [
-                5,
-                1,
-                4,
-                4,
-                5,
-                1,
-                2,
-                5,
-                4,
-                1
-            ]
-};
+var profileArray = [
+    {
+      "userName": "GamerGrill",
+      "favoriteGame": "Call of Duty",
+      "userPhoto": "",
+      "scores": [5,5,5,5,5,5,5,5,5,5]
+    }];
+
+    module.exports = profileArray;
